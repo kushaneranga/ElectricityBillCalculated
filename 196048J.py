7b920c948400a1e09131c5e741f54c9d45d6dd0b
Kushan Eranga <61194721+kushaneranga@users.noreply.github.com>
2020-08-16 06:08:52 +0530

    Add files via upload

---
+++ 196048J.py
@@ -0,0 +1,10 @@
+print("***Electric bill calculation***")
+units = float(input("Number of Units (KWH): "))
+if units <= 50:
+    amount = units * 5
+elif 50 < units <= 150:
+    amount = (50 * 5) + (units - 150) * 7
+elif units > 150:
+    amount = (50 * 5) + (150 - 50) * 7 + (units - 150) * 10
+TAX = float(amount) * 20 / 100
+print("Your bill Rs: ", amount + TAX)
